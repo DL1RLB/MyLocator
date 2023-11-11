30cc7906c5be81428c891a8536d7517cb9f79f72
Lutz Bär <69315366+DL1RLB@users.noreply.github.com>
2023-11-11 14:18:16 +0100

    mit Kompass aber erstmal nur mit Festwerte

--- MyLocator/Form1.Designer.cs
+++ MyLocator/Form1.Designer.cs
@@ -39,12 +39,15 @@
             this.label2entfernung = new System.Windows.Forms.Label();
             this.lblZielKoordinaten = new System.Windows.Forms.Label();
             this.lblEigeneKoordinaten = new System.Windows.Forms.Label();
+            this.picHeading = new System.Windows.Forms.PictureBox();
+            this.lblDegrees = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picHeading)).BeginInit();
             this.SuspendLayout();
             // 
             // heimatlocator
             // 
             this.heimatlocator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.heimatlocator.Location = new System.Drawing.Point(94, 13);
+            this.heimatlocator.Location = new System.Drawing.Point(78, 13);
             this.heimatlocator.Name = "heimatlocator";
             this.heimatlocator.Size = new System.Drawing.Size(100, 26);
             this.heimatlocator.TabIndex = 0;
@@ -55,7 +58,7 @@
             // ziellocator
             // 
             this.ziellocator.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.ziellocator.Location = new System.Drawing.Point(94, 45);
+            this.ziellocator.Location = new System.Drawing.Point(78, 45);
             this.ziellocator.Name = "ziellocator";
             this.ziellocator.Size = new System.Drawing.Size(100, 26);
             this.ziellocator.TabIndex = 1;
@@ -65,7 +68,7 @@
             // 
             // Berechnen
             // 
-            this.Berechnen.Location = new System.Drawing.Point(240, 115);
+            this.Berechnen.Location = new System.Drawing.Point(87, 141);
             this.Berechnen.Name = "Berechnen";
             this.Berechnen.Size = new System.Drawing.Size(75, 23);
             this.Berechnen.TabIndex = 4;
@@ -75,7 +78,7 @@
             // tbRichtung
             // 
             this.tbRichtung.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbRichtung.Location = new System.Drawing.Point(94, 77);
+            this.tbRichtung.Location = new System.Drawing.Point(78, 77);
             this.tbRichtung.Name = "tbRichtung";
             this.tbRichtung.Size = new System.Drawing.Size(100, 26);
             this.tbRichtung.TabIndex = 3;
@@ -84,7 +87,7 @@
             // tbEntfernung
             // 
             this.tbEntfernung.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbEntfernung.Location = new System.Drawing.Point(94, 109);
+            this.tbEntfernung.Location = new System.Drawing.Point(78, 109);
             this.tbEntfernung.Name = "tbEntfernung";
             this.tbEntfernung.Size = new System.Drawing.Size(101, 26);
             this.tbEntfernung.TabIndex = 5;
@@ -93,7 +96,7 @@
             // label1meinlocator
             // 
             this.label1meinlocator.AutoSize = true;
-            this.label1meinlocator.Location = new System.Drawing.Point(12, 21);
+            this.label1meinlocator.Location = new System.Drawing.Point(3, 21);
             this.label1meinlocator.Name = "label1meinlocator";
             this.label1meinlocator.Size = new System.Drawing.Size(69, 13);
             this.label1meinlocator.TabIndex = 8;
@@ -103,7 +106,7 @@
             // label1andererlocator
             // 
             this.label1andererlocator.AutoSize = true;
-            this.label1andererlocator.Location = new System.Drawing.Point(57, 56);
+            this.label1andererlocator.Location = new System.Drawing.Point(48, 56);
             this.label1andererlocator.Name = "label1andererlocator";
             this.label1andererlocator.Size = new System.Drawing.Size(24, 13);
             this.label1andererlocator.TabIndex = 8;
@@ -113,7 +116,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 88);
+            this.label1.Location = new System.Drawing.Point(22, 90);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(50, 13);
             this.label1.TabIndex = 9;
@@ -122,7 +125,7 @@
             // label2entfernung
             // 
             this.label2entfernung.AutoSize = true;
-            this.label2entfernung.Location = new System.Drawing.Point(22, 120);
+            this.label2entfernung.Location = new System.Drawing.Point(13, 122);
             this.label2entfernung.Name = "label2entfernung";
             this.label2entfernung.Size = new System.Drawing.Size(59, 13);
             this.label2entfernung.TabIndex = 9;
@@ -132,7 +135,7 @@
             // lblZielKoordinaten
             // 
             this.lblZielKoordinaten.AutoSize = true;
-            this.lblZielKoordinaten.Location = new System.Drawing.Point(200, 56);
+            this.lblZielKoordinaten.Location = new System.Drawing.Point(184, 56);
             this.lblZielKoordinaten.Name = "lblZielKoordinaten";
             this.lblZielKoordinaten.Size = new System.Drawing.Size(64, 13);
             this.lblZielKoordinaten.TabIndex = 10;
@@ -142,18 +145,40 @@
             // lblEigeneKoordinaten
             // 
             this.lblEigeneKoordinaten.AutoSize = true;
-            this.lblEigeneKoordinaten.Location = new System.Drawing.Point(200, 21);
+            this.lblEigeneKoordinaten.Location = new System.Drawing.Point(184, 21);
             this.lblEigeneKoordinaten.Name = "lblEigeneKoordinaten";
             this.lblEigeneKoordinaten.Size = new System.Drawing.Size(64, 13);
             this.lblEigeneKoordinaten.TabIndex = 11;
             this.lblEigeneKoordinaten.Text = "Koordinaten";
             this.lblEigeneKoordinaten.Click += new System.EventHandler(this.lblEigeneKoordinaten_Click);
             // 
+            // picHeading
+            // 
+            this.picHeading.Location = new System.Drawing.Point(297, 12);
+            this.picHeading.Name = "picHeading";
+            this.picHeading.Size = new System.Drawing.Size(260, 260);
+            this.picHeading.TabIndex = 12;
+            this.picHeading.TabStop = false;
+            this.picHeading.LocationChanged += new System.EventHandler(this.Berechnen_Click);
+            this.picHeading.Paint += new System.Windows.Forms.PaintEventHandler(this.picHeading_Paint);
+            // 
+            // lblDegrees
+            // 
+            this.lblDegrees.AutoSize = true;
+            this.lblDegrees.Location = new System.Drawing.Point(507, 275);
+            this.lblDegrees.Name = "lblDegrees";
+            this.lblDegrees.Size = new System.Drawing.Size(50, 13);
+            this.lblDegrees.TabIndex = 13;
+            this.lblDegrees.Text = "Richtung";
+            this.lblDegrees.Click += new System.EventHandler(this.lblDegrees_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(341, 157);
+            this.ClientSize = new System.Drawing.Size(569, 288);
+            this.Controls.Add(this.lblDegrees);
+            this.Controls.Add(this.picHeading);
             this.Controls.Add(this.lblEigeneKoordinaten);
             this.Controls.Add(this.lblZielKoordinaten);
             this.Controls.Add(this.label2entfernung);
@@ -168,6 +193,7 @@
             this.Name = "Form1";
             this.Text = "MyLocator";
             this.Load += new System.EventHandler(this.Berechnen_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.picHeading)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,6 +212,8 @@
         private System.Windows.Forms.Label label2entfernung;
         private System.Windows.Forms.Label lblZielKoordinaten;
         private System.Windows.Forms.Label lblEigeneKoordinaten;
+        private System.Windows.Forms.PictureBox picHeading;
+        private System.Windows.Forms.Label lblDegrees;
     }
 }
 
--- MyLocator/Form1.cs
+++ MyLocator/Form1.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Drawing;
 using System.Windows.Forms;
 
 
@@ -11,16 +14,20 @@ namespace MyLocator
             InitializeComponent();
         }
 
+      //  int CurrentValue = 90;
 
-    private void Berechnen_Click(object sender, EventArgs e)
+        public void Berechnen_Click(object sender, EventArgs e)
         {
-            // Entfernung berechnen aus zwei Textfeldern
-            double entfernung = MaidenheadLocator.Distance(heimatlocator.Text, ziellocator.Text);
+            
+
+        // Entfernung berechnen aus zwei Textfeldern
+        double entfernung = MaidenheadLocator.Distance(heimatlocator.Text, ziellocator.Text);
             
             // Richtung berechnen aus zwei Textfeldern
             double richtung = MaidenheadLocator.Azimuth(heimatlocator.Text, ziellocator.Text);
             
-            
+            int CurrentValueRichtung = Convert.ToInt32(CurrentValue);
+
             // Heimatlocator aus dem Textfeld
             string heimatLocator = heimatlocator.Text;
 
@@ -32,12 +39,159 @@ namespace MyLocator
             string myLoc = MaidenheadLocator.LatLngToLocator(52.41, 12.55);
 
             tbRichtung.Text = Math.Round(double.Parse(richtung.ToString()),2).ToString() +" °";
+            lblDegrees.Text = CurrentValueRichtung.ToString();
+
             tbEntfernung.Text = Math.Round(double.Parse(entfernung.ToString()),2).ToString() + " km";
            // tbKoordinaten.Text = heimatkoordinaten.ToString();
            // tbHeimatlocator.Text = heimatLocator.ToString();
+            
             lblZielKoordinaten.Text = zielkoordinaten.ToString();
+            
             lblEigeneKoordinaten.Text = heimatkoordinaten.ToString();
+
         }
+
+       
+        int CurrentValue = 80;
+
+
+        #region runder Kompass
+        private void picHeading_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(picHeading.BackColor);
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.TextRenderingHint = TextRenderingHint.AntiAlias;
+
+            // Draw the heading picture.
+            using (Font nsew_font = new Font("Times New Roman", 18,
+                FontStyle.Bold))
+            {
+                DrawHeading(e.Graphics, CurrentValue, nsew_font);
+            }
+        }
+        #endregion
+
+        #region Grad Wert
+        //private void Richtungszahl(object sender, EventArgs e)
+        //{
+        //    CurrentValue = 75;
+        //    lblDegrees.Text = "hä";
+
+        //    //picCompass.Refresh();
+        //    picHeading.Refresh();
+        //}
+        #endregion
+
+        #region runder Kompass
+        private void DrawHeading(Graphics gr, int value, Font font)
+        {
+            float cx = picHeading.ClientSize.Width / 2f;
+            float cy = picHeading.ClientSize.Height / 2f;
+
+            // Draw NSEW.
+            float letter_r = Math.Min(cx, cy) * 0.85f;
+            string[] letters = { "0°", "90°", "180°", "270°" };
+            int[] degrees = { 270, 0, 90, 180 };
+            for (int i = 0; i < 4; i++)
+            {
+                float letter_x = letter_r * (float)Math.Cos(DegreesToRadians(degrees[i]));
+                float letter_y = letter_r * (float)Math.Sin(DegreesToRadians(degrees[i]));
+                PointF point = new PointF(cx + letter_x, cy + letter_y);
+                DrawRotatedText(gr, font, Brushes.Black,
+                    letters[i], point, degrees[i] + 90);
+            }
+
+            // Draw tick marks.
+            const int large_tick_freq = 30; // Draw a large tick mark every 30 degrees.
+            const int small_tick_freq = 15; // Draw a small tick mark every 15 degrees.
+            const int tiny_tick_freq = 3;   // Draw a tiny tick mark every 3 degrees.
+            float outer_r = letter_r * 0.9f;
+            float large_r = outer_r * 0.8f;
+            float small_r = outer_r * 0.9f;
+            float tiny_r = outer_r * 0.95f;
+            using (Pen pen = new Pen(Color.Blue, 3))
+            {
+                for (int i = tiny_tick_freq; i <= 360; i += tiny_tick_freq)
+                {
+                    float cos = (float)Math.Cos(DegreesToRadians(i));
+                    float sin = (float)Math.Sin(DegreesToRadians(i));
+                    float x0 = cx + outer_r * cos;
+                    float y0 = cy + outer_r * sin;
+
+                    float x1, y1;
+                    if (i % large_tick_freq == 0)
+                    {
+                        pen.Width = 3;
+                        x1 = cx + large_r * cos;
+                        y1 = cy + large_r * sin;
+                    }
+                    else if (i % small_tick_freq == 0)
+                    {
+                        pen.Width = 2;
+                        x1 = cx + small_r * cos;
+                        y1 = cy + small_r * sin;
+                    }
+                    else
+                    {
+                        pen.Width = 1;
+                        x1 = cx + tiny_r * cos;
+                        y1 = cy + tiny_r * sin;
+                    }
+                    gr.DrawLine(pen, x0, y0, x1, y1);
+                }
+            }
+
+            // Draw the pointer.
+            // Rotate 90 degrees so North is at 0.
+            double radians = DegreesToRadians(value - 90);
+
+            const int tip_r = 4;
+            float pointer_r = large_r * 1.0f;
+            float tip_x = cx + pointer_r * (float)Math.Cos(radians);
+            float tip_y = cx + pointer_r * (float)Math.Sin(radians);
+            float tip_x1 = cx + tip_r * (float)Math.Cos(radians + Math.PI / 2.0);
+            float tip_y1 = cy + tip_r * (float)Math.Sin(radians + Math.PI / 2.0);
+            float tip_x2 = cx + tip_r * (float)Math.Cos(radians - Math.PI / 2.0);
+            float tip_y2 = cy + tip_r * (float)Math.Sin(radians - Math.PI / 2.0);
+            PointF[] points =
+            {
+                new PointF(tip_x, tip_y),
+                new PointF(tip_x1, tip_y1),
+                new PointF(tip_x2, tip_y2),
+            };
+            gr.FillPolygon(Brushes.Black, points);
+
+            // Draw the center.
+            const int center_r = 6;
+            RectangleF rect = new RectangleF(
+                cx - center_r, cy - center_r,
+                2 * center_r, 2 * center_r);
+            gr.FillEllipse(Brushes.LightBlue, rect);
+            gr.DrawEllipse(Pens.Black, rect);
+        }
+        #endregion
+
+        private double DegreesToRadians(float degrees)
+        {
+            return degrees * Math.PI / 180;
+        }
+        #region zeiger im runden Kompass
+        private void DrawRotatedText(Graphics gr, Font font, Brush brush,
+            string text, PointF location, float degrees)
+        {
+            GraphicsState state = gr.Save();
+            gr.ResetTransform();
+            gr.RotateTransform(degrees);
+            gr.TranslateTransform(location.X, location.Y, MatrixOrder.Append);
+            using (StringFormat sf = new StringFormat())
+            {
+                sf.Alignment = StringAlignment.Center;
+                sf.LineAlignment = StringAlignment.Center;
+                gr.DrawString(text, font, brush, 0, 0, sf);
+            }
+            gr.Restore(state);
+        }
+        #endregion
 
         #region alte einträge
         // Zwei Locatorangaben definieren
@@ -49,7 +203,7 @@ namespace MyLocator
 
         // Locator in Breite/Länge konvertieren
         //     LatLng homeLL = MaidenheadLocator.LocatorToLatLng(homeLoc);
-          //string myLoc = MaidenheadLocator.LatLngToLocator(49.57, 11.08);
+        //string myLoc = MaidenheadLocator.LatLngToLocator(49.57, 11.08);
 
         #endregion
 
@@ -101,5 +255,10 @@ namespace MyLocator
 
         }
         #endregion
+
+        private void lblDegrees_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
